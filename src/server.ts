20f546170c1872280980fb8de588c20427c33f9f
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-07-16 04:47:18 +0600

    chore(db): initialize MongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
